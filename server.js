dd306fde4a7d3f27d86e3c0b1e0c8017c874eb0f
Ana URL <ana.urlic@gmail.com>
2022-11-10 16:21:06 +0100

    Fix responsiveness

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express') //making it possible to use express in this file
-const app = express() //creating an express app
-const PORT = 8000  //setting a port to 8000
-const mongoose = require('mongoose')  //requiring mongoose 
+const express = require('express') 
+const app = express() 
+const PORT = 3000 
+const mongoose = require('mongoose') 
 
-const Microagression = require('./models/microagressions') // this is where I get the model
-require ('dotenv').config() //allows us to look for vars inside the .env file
+const Microagression = require('./models/microagressions') 
+require ('dotenv').config() 
 
 //Set middleware
 app.set('view engine', 'ejs')
@@ -67,19 +67,7 @@ app.get('/more-info', (request, response) => {
     response.render("more-info.ejs") 
 
 }); 
-// preview of each microagression - returns 500 
-app
-    .route("/examples/:id")
-    .get((request,response) => {
-        const id = request.params.id 
-        console.log(mongoose.isValidObjectId(request.params.id))
-        Microagression.findById({}, (err, microagressions) => {
-            if (err) return response.status(500).send(err)
-            response.render('examples.ejs', { 
-                microagressionsList:microagressions, 
-                idMicroagression:id })
-        })
-        });
+
 
 
 //POST - ADD
@@ -114,8 +102,8 @@ app
 })
 
 
-.post ((request, response) => { //find and update
-    const id = request.params.id //we need to send id so the db knows which item we are looking for 
+.post ((request, response) => { 
+    const id = request.params.id 
     Microagression.findByIdAndUpdate ( //Finds a matching document, updates it according to the update arg, and returns the found document 
     //(if any) to the callback. The query executes if callback is passed.
         id,  //ID ARGUMENT
@@ -131,16 +119,16 @@ app
 })
 
 //DELETE
-app
-        .route("/remove/:id") 
-        . get((request, response) => {
-            const id = request.params.id //assigning query id to a id variable
-            Microagression.findByIdAndRemove(id, err => { //Finds a matching document by id, removes it, passing the found document (if any) to the callback.
-                                                        //Executes the query if callback is passed.
-			if (err) return response.status(500).send(err) 
-			response.redirect("/")
-		})
-	})
+// app
+//         .route("/remove/:id") 
+//         . get((request, response) => {
+//             const id = request.params.id //assigning query id to a id variable
+//             Microagression.findByIdAndRemove(id, err => { //Finds a matching document by id, removes it, passing the found document (if any) to the callback.
+//                                                         //Executes the query if callback is passed.
+// 			if (err) return response.status(500).send(err) 
+// 			response.redirect("/")
+// 		})
+// 	})
 
 
 //Start server
