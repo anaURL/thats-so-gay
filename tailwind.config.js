16e0194910bcdf34a0945cdd82eb1e269a5ac2f5
Ana URL <ana.urlic@gmail.com>
2022-11-10 18:23:22 +0100

    purge css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./views/*.ejs"],
+    mode: 'jit',
+   purge: [
+     './views/*.ejs',
+   ],
     theme: {
       extend: { 
       }
